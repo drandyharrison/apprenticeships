4d1bc1167fa2f69daf87fdc0fa75970583d89ebb
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-10 20:50:16 +0000

    Read urls to process from JSON
    Also tidied up the comments

--- apprenticeships.py
+++ apprenticeships.py
@@ -2,6 +2,7 @@
 # the articles uses the DfE statistics at https://www.gov.uk/government/statistics/apprenticeships-in-england-by-industry-characteristics
 # ---------------------------------------------------------------------
 import sys
+import os
 import xlrd
 import pandas as pd
 from urllib.request import urlopen
@@ -9,14 +10,20 @@ import matplotlib
 import validators
 from http.client import OK, FOUND, MOVED_PERMANENTLY
 import requests
+import json
+from pathlib import Path
 
+# TODO read from a JSON file
 # url for data, which links to an Excel
-data_url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/750709/apprenticeship_starts_tables.xlsx"
+# JSON file containing the data urls to process
+json_fname_of_urls = 'data_url.json'
+#data_url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/750709/apprenticeship_starts_tables.xlsx"
 
 def check_url(url):
     """
-    Check if a URL exists without downloading the whole file.
-    We only check the URL header.
+    Check if a URL exists without downloading the whole file. It only checks the URL header.
+    :param url: the url to be checked
+    :return:    True if url is a valid url string and web site exists/responds
     """
     # check it's a valid url string
     if validators.url(url):
@@ -39,18 +46,13 @@ def check_url(url):
         print("@check_url() Invalid url: {}".format(url))
         return False
 
-# Read an Excel workbook from a given url
-#   url     url pointing to Excel
-#
-# returns contents of Excel as a pandas dataframe
-#
 def get_xlsx_from_url(url):
     """
     Read an Excel workbook from a url
-    :param url:
-    :return:
+    :param url: url pointing to an Excel workbook
+    :return:    contents of Excel as a pandas dataframe
     """
-
+    # is the url valid?
     if check_url(url):
         # open url
         try:
@@ -75,8 +77,32 @@ def get_xlsx_from_url(url):
         raise ValueError("URL doesn't exist")
 
 # TODO - create unit test based on these
-url_list = ["google", "http://google.co.uk", "https://www.shddf.xx.xx"]
-url_list.append(data_url)
+url_list = []
+# read urls to process from a JSON file
+json_of_urls = Path(json_fname_of_urls)   # create Path object
+# does file exist
+if json_of_urls.exists():
+    # check it's a file
+    if json_of_urls.is_file():
+        with json_of_urls.open() as f:
+            try:
+                json_data = json.load(f)
+            except json.decoder.JSONDecodeError as e:
+                print("json.decoder.JSONDecodeError: reading {} | error: {}".format(json_fname_of_urls, e.msg))
+                quit(-1)
+            except:
+                print("@check_url() Unexpected error (when converting string to integer):", sys.exc_info()[0])
+                quit(-1)
+            else:
+                for data_url in json_data:
+                    print("data url: {}".format(data_url))
+                    url_list.append(data_url)
+    else:
+        print("Isn't a file: {}".format(json_fname_of_urls))
+else:
+    print("Doesn't exist: {}".format(json_fname_of_urls))
+
+# process the urls
 for url in url_list:
     print("Processing: {}".format(url), flush=True)
     try:
@@ -84,18 +110,15 @@ for url in url_list:
     except ValueError as e:
         print("\tFailed", flush=True)
     else:
-        print("\tSheet names: {}".format(xlsx_data.sheet_names), flush=True)
+        # get the sheet names
+        sht_names = xlsx_data.sheet_names
+        print("\tSheet names: {}".format(sht_names), flush=True)
+
 ## temporary exit to check code
 quit(0)
 
 # TODO add exeption handling
-# create socket for url
-socket = urlopen(data_url)
-# get Excel workbook
-xlsx_data = pd.ExcelFile(socket)
-# get the sheet names
-sht_names = xlsx_data.sheet_names
-print(sht_names)
+
 # Load worksheet 1A into a data frame
 df = xlsx_data.parse("1A")
 # size and shape of the dataframe
