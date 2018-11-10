a2f85092c541815b56e3768e80c6b7bdfa04c4c5
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-10 18:12:22 +0000

    check_url working
    Checks whether the passed url is a valid url string, the corresponding website exists and points to an Excel workbook

--- apprenticeships.py
+++ apprenticeships.py
@@ -7,9 +7,37 @@ import pandas as pd
 from urllib.request import urlopen
 import matplotlib
 import validators
+from http.client import OK, FOUND, MOVED_PERMANENTLY
+import requests
 
 # url for data, which links to an Excel
 data_url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/750709/apprenticeship_starts_tables.xlsx"
+
+def check_url(url):
+    """
+    Check if a URL exists without downloading the whole file.
+    We only check the URL header.
+    """
+    # check it's a valid url string
+    if validators.url(url):
+        good_codes = [OK, FOUND, MOVED_PERMANENTLY]
+        # check url exists
+        try:
+            request = requests.get(url)
+            if request.status_code in good_codes:
+                return True
+            else:
+                print("@check_url() Website returned response code: {code}".format(code=request.status_code))
+                return False
+        except requests.exceptions.ConnectionError as e:
+            print('@check_url() Connection error for {}'.format(url))
+            return False
+        except:
+            print("@check_url() Unexpected error (when converting string to integer):", sys.exc_info()[0])
+            return False
+    else:
+        print("@check_url() Invalid url: {}".format(url))
+        return False
 
 # Read an Excel workbook from a given url
 #   url     url pointing to Excel
@@ -22,12 +50,16 @@ def get_xlsx_from_url(url):
     :param url:
     :return:
     """
-    if validators.url(url):
+
+    if check_url(url):
         # open url
         try:
             socket = urlopen(url)
         except TimeoutError as e:
             print('Timeout error for {}'.format(url))
+            raise ValueError("url probably doesn't exist")
+        except requests.exceptions.ConnectionError as e:
+            print('Connection error for {}'.format(url))
             raise ValueError("url probably doesn't exist")
         except:
             print("Unexpected error (when converting string to integer):", sys.exc_info()[0])
@@ -39,19 +71,20 @@ def get_xlsx_from_url(url):
             raise ValueError("Not an xlsx file: {}".format(url))
         return xlsx_data
     else:
-        raise ValueError("Invalid url: {}".format(url))
+        # TODO change to return False not raising an error
+        raise ValueError("URL doesn't exist")
 
 # TODO - create unit test based on these
-url_list = ["google", "http://google.co.uk"] #, "https://www.shddf.xx.xx"]
+url_list = ["google", "http://google.co.uk", "https://www.shddf.xx.xx"]
 url_list.append(data_url)
 for url in url_list:
-    print("Processing: {}".format(url))
+    print("Processing: {}".format(url), flush=True)
     try:
         xlsx_data = get_xlsx_from_url(url)
     except ValueError as e:
-        print("\tFailed")
+        print("\tFailed", flush=True)
     else:
-        print("\tSheet names: {}".format(xlsx_data.sheet_names))
+        print("\tSheet names: {}".format(xlsx_data.sheet_names), flush=True)
 ## temporary exit to check code
 quit(0)
 
