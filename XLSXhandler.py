59efbac74cde2301bcef24fe233d1f42db5aef4d
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-17 21:27:30 +0000

    Added in a creator function with fname

--- XLSXhandler.py
+++ XLSXhandler.py
@@ -5,3 +5,9 @@ class XLSXhandler:
     def __init__(self):
         pass
 
+    def __init__(self, fname):
+        if (isinstance(fname, str)):
+            # the name of the Excel file to be processed - can be a filename, full path, url etc.
+            self.fname = fname
+        else:
+            raise ValueError("@XLSXhandler creator: {} is not a string".format(fname))
