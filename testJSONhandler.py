861f0eb885fb8db6163ec89257d2df4e983fc7a7
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-19 22:10:05 +0000

    Started to add unit tests

--- testJSONhandler.py
+++ testJSONhandler.py
@@ -2,7 +2,23 @@ import unittest
 from JSONhandler import JSONhandler
 
 class testJSONhandler(unittest.TestCase):
-    pass
+    def SetUp(self):
+        """set-up code, which is called before each test, to avoid repetition"""
+        pass
+
+    def tearDown(self):
+        """code to tidy up after each test"""
+        pass
+
+    def test_creator_notstr(self):
+        """check that creator throws expected error when passed a non-string"""
+        self.assertRaises(ValueError, JSONhandler, 25)
+
+    def test_example(self):
+        pass
+        # self.assertEquals
+        # assertTrue - assertFalse
+        # assertRaises
 
 # run tests
 if __name__ == '__main__':
--- testURLhandler.py
+++ testURLhandler.py
@@ -2,8 +2,21 @@ import unittest
 from URLhandler import URLhandler
 
 class testURLhandler(unittest.TestCase):
-    pass
+    def SetUp(self):
+        """set-up code, which is called before each test, to avoid repetition"""
+        pass
+
+    def tearDown(self):
+        """code to tidy up after each test"""
+        pass
+
+    def test_example(self):
+        pass
+        # assertEquals
+        # assertTrue - assertFalse
+        # assertRaises
+
 
 # run tests
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
