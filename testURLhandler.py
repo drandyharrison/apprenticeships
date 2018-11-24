0a748488f47840694cadf4ef0eeed74adcc9acd5
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-24 19:36:56 +0000

    Defined unit tests

--- testURLhandler.py
+++ testURLhandler.py
@@ -10,6 +10,25 @@ class testURLhandler(unittest.TestCase):
         """code to tidy up after each test"""
         pass
 
+    def test_creator(self):
+        """Test that creator throws a ValueError for a non-string url"""
+        self.assertRaises(ValueError, URLhandler, 25)
+
+    def test_invalid_url(self):
+        """Checks that a badly url returns False when checked"""
+        urlhndlr = URLhandler("google")
+        self.assertFalse(urlhndlr.check_url())
+
+    def test_non_exist_url(self):
+        """Checks that a well formed url that doesn't exist returns False when checked"""
+        urlhndlr = URLhandler("https://www.shddf.xx.xx")
+        self.assertFalse(urlhndlr.check_url())
+
+    def test_exist_url(self):
+        """Checks that an valid url returns Trie when checked"""
+        urlhndlr = URLhandler("https://www.google.co.uk")
+        self.assertTrue(urlhndlr.check_url())
+
     def test_example(self):
         pass
         # assertEquals
