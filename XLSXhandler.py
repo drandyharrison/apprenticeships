9afcaa1ab5cfc44ea1d688083f06ab4b1f3181c3
Andy Harrison <iam.andyharrison@gmail.com>
2018-11-24 20:21:19 +0000

    Added test for empty or blank filename in creator

--- XLSXhandler.py
+++ XLSXhandler.py
@@ -14,8 +14,12 @@ class XLSXhandler:
 
     def __init__(self, fname):
         if (isinstance(fname, str)):
-            # the name of the Excel file to be processed - can be a filename, full path, url etc.
-            self.fname = fname
+            # check whether string is empty or blanh
+            if not(fname and fname.strip()):
+                raise ValueError("@XLSXhandler creator: {} is blank".format(jsonfname_fname))
+            else:
+                # the name of the Excel file to be processed - can be a filename, full path, url etc.
+                self.fname = fname
         else:
             raise ValueError("@XLSXhandler creator: {} is not a string".format(fname))
 
