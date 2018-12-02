e874207a87d9a29fbe1a56b444363b4b072461e5
Andy Harrison <iam.andyharrison@gmail.com>
2018-12-02 22:07:29 +0000

    Draw a bar chart

--- apprenticeships.py
+++ apprenticeships.py
@@ -2,7 +2,6 @@
 # the articles uses the DfE statistics at https://www.gov.uk/government/statistics/apprenticeships-in-england-by-industry-characteristics
 # ---------------------------------------------------------------------
 import pandas
-import numpy
 import matplotlib.pyplot as plt
 from XLSXhandler import XLSXhandler
 
@@ -19,14 +18,13 @@ for url in jsondf.values:
         sht_names = xlsx.get_sheet_names()  # get the sheet names
         print("\tSheet names: {}".format(sht_names), flush=True)
         xlsx.extract_worksheet_data("1A", 3, 4, 7, 28, 5)
-        # Simple plot of totals
-        plt.plot(xlsx.hdr_labels, xlsx.totals, 'ro')
+        # create bar chart
+        # TODO add hover text for values on bars
+        plt.bar(xlsx.hdr_labels, xlsx.totals, 1/1.5, color='green')
         plt.xlabel('Years')
-        plt.ylabel('Total')
-        plt.title('Apprenticeships')
+        plt.title('Apprenticeships (totals)')
         # TODO How to make bigger to accomomdate y-axis label
         plt.show()
-        # TODO plot a histogram of the data
         # TODO create a function to create a histogram for the analysed data - so will plot any data
         # TODO process the other worksheets to replicate the FEweek analysis
     else:
