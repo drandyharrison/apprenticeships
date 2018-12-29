e6f3c20ea7c503c164fe4a61cd5f8dfb9a691a9a
Andy Harrison <iam.andyharrison@gmail.com>
2018-12-29 13:14:49 +0000

    Added validation of the nonnumeric and cast parameters
    And associated unit tests

--- replace_nonnumeric.py
+++ replace_nonnumeric.py
@@ -8,13 +8,14 @@ def replace_nonnumeric(data, nonumeric=numpy.nan, cast=False):
     nonumeric - replace non-numeric values with this value
     cast      - attempt to cast non-numeric values; e.g. a string like '123'"""
     # validate inputs
-    # TODO check data is iterable
     try:
         iterator = iter(data)
     except TypeError:
-        raise TypeError("@replace_nonnumeric: data are not iterable")
-    # TODO check nonnumeric is numeric
-    # TODO check cast is boolean
+        raise TypeError("@replace_nonnumeric: data is not iterable")
+    if not (isinstance(nonumeric, int) or isinstance(nonumeric, float)):
+        raise TypeError("@replace_nonnumeric: nonumeric is not numeric")
+    if not isinstance(cast, bool):
+        raise TypeError("@replace_nonnumeric: cast is not boolean")
     # TODO implement the cast flag
     for idx, val in enumerate(data):
         if not (isinstance(val, int) or isinstance(val, float)):
--- test_replace_nonnumeric.py
+++ test_replace_nonnumeric.py
@@ -12,7 +12,7 @@ class testReplaceNonnumeric(unittest.TestCase):
         pass
 
     def test_replace_nonnumeric_data_not_iterable(self):
-        """Check replace_nonnumeric throws a ValueError if something happens"""
+        """Check replace_nonnumeric throws a TypeError if data is not iterable"""
         print("@test_replace_nonnumeric_template")
         # arrange
         test_data = 2
@@ -22,6 +22,27 @@ class testReplaceNonnumeric(unittest.TestCase):
         # asset
         self.assertRaises(TypeError, replace_nonnumeric, test_data, nonnumeric=nonnumeric_val, cast=cast_val)
 
+    def test_replace_nonnumeric_nonnumeric_not_numeric(self):
+        """Check replace_nonnumeric throws a TypeError if nonnumeric is not numeric"""
+        print("@test_replace_nonnumeric_nonnumeric_not_numeric")
+        # arrange
+        test_data = 2
+        nonnumeric_val = "nan"
+        cast_val = False
+        # act
+        # asset
+        self.assertRaises(TypeError, replace_nonnumeric, test_data, nonnumeric=nonnumeric_val, cast=cast_val)
+
+    def test_replace_nonnumeric_cast_not_boolean(self):
+        """Check replace_nonnumeric throws a TypeError if cast is not boolean"""
+        print("@test_replace_nonnumeric_cast_not_boolean")
+        # arrange
+        test_data = 2
+        nonnumeric_val = numpy.nan
+        cast_val = "x"
+        # act
+        # asset
+        self.assertRaises(TypeError, replace_nonnumeric, test_data, nonnumeric=nonnumeric_val, cast=cast_val)
 
 # run tests
 if __name__ == '__main__':
