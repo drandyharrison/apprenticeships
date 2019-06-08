104ecb8df4abcbd48f17020dfd48eee38550da56
Andy Harrison <iam.andyharrison@gmail.com>
2019-06-08 18:00:53 +0100

    Changes to reflect refactoring of XLSXhandler

--- apprenticeships.py
+++ apprenticeships.py
@@ -22,7 +22,7 @@ for url in jsondf.values:
         sht_names = xlsx.get_sheet_names()  # get the sheet names
         print("\tSheet names: {}".format(sht_names), flush=True)
         # Get apprenticeship starts by sector
-        xlsx.extract_worksheet_data("1A", 3, 4, 7, 28, 5)
+        xlsx.extract_worksheet_data("1A", 5, 6, 9, 30, 5)
         title = 'Change in apprenticeships starts by sector'
         width = 0.9
         fig_id = 1
@@ -33,23 +33,21 @@ for url in jsondf.values:
         fig.canvas.set_window_title('Apprenticeships')     # name the figure
         # if called with the wrong type of parameters, it will raise a TypeError
         # TODO (1) Health and Social work dominate
-        x_data = xlsx.row_labels.values
+        x_data = xlsx.get_row_labels()
         for idx, x_val in enumerate(x_data):
             x_data[idx] = x_val.strip()             # remove any leading or trailing spaces from labels
         # create y_data array, with multiple rows
-        # check same shape
-        if numpy.shape(xlsx.data[:, 0]) != numpy.shape(xlsx.data[:, 4]):
-            raise ValueError("@appenticeships: data sets not same shape")
-        num_cols = numpy.size(xlsx.data, 0)
-        y_data = numpy.zeros(shape=(2, num_cols), dtype=numpy.float64)
+        y_data = numpy.zeros(shape=(2, xlsx.get_num_rows()), dtype=numpy.float64)
         # replace an non-numeric values
-        y_data_1213 = xlsx.data[:, 0]
-        y_data_1617 = xlsx.data[:, 4]
+        y_data_1213 = numpy.array(numpy.asmatrix(xlsx.get_col_val(0))).flatten()
+        y_data_1617 = numpy.array(numpy.asmatrix(xlsx.get_col_val(4))).flatten()
         # remove non-numeric values before populating the numpy array of floats
+        # TODO now I know Python is there existing functionality to do this
         replace_nonnumeric.replace_nonnumeric(y_data_1213)
         replace_nonnumeric.replace_nonnumeric(y_data_1617)
         y_data[0, :] = y_data_1213
         y_data[1, :] = y_data_1617
+        # TODO print the dataframe as a whole should make it a multiple bar chart with legends
         create_barchart.create_barchart(x_data, y_data/1000, width, ['red', 'blue'], 'Sectors', "", fig_id, 221, "Number (000s)",
                         ['2012/12', '2016/17'], True, 's')
         # create_barchart.create_barchart(x_data, y_data/1000, width, ['red', 'blue'], 'Sectors', "", fig_id, 221, True, 'h')
